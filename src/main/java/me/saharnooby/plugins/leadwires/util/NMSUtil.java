a0d9d42998e4048dc623d061d91768e9e4a39735
saharNooby <saha-ha98@yandex.ru>
2020-09-08 18:18:01 +0500

    Rewrite server version detection

--- src/main/java/me/saharnooby/plugins/leadwires/util/NMSUtil.java
+++ src/main/java/me/saharnooby/plugins/leadwires/util/NMSUtil.java
@@ -3,23 +3,30 @@ package me.saharnooby.plugins.leadwires.util;
 import lombok.NonNull;
 import org.bukkit.Bukkit;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class NMSUtil {
 
 	private static final String VERSION;
-	private static final int MINOR_VERSION;
+	private static final int MINOR;
+	private static final int RELEASE;
 
 	static {
 		String name = Bukkit.getServer().getClass().getName();
 
 		VERSION = name.substring(23, name.lastIndexOf('.'));
 
-		if (!VERSION.matches("v1_\\d{1,2}_R\\d{1,2}")) {
-			throw new IllegalStateException("Invalid server version '" + VERSION + "', server class is " + name);
+		Pattern pattern = Pattern.compile("v1_(\\d{1,2})_R(\\d{1,2})");
+
+		Matcher matcher = pattern.matcher(VERSION);
+
+		if (!matcher.matches()) {
+			throw new IllegalStateException("Invalid server version \"" + VERSION + "\", server class is \"" + name + "\"");
 		}
 
-		String version = VERSION;
-		version = version.substring(version.indexOf('_') + 1);
-		MINOR_VERSION = Integer.parseInt(version.substring(0, version.indexOf('_')));
+		MINOR = Integer.parseInt(matcher.group(1));
+		RELEASE = Integer.parseInt(matcher.group(2));
 	}
 
 	/**
@@ -33,7 +40,14 @@ public final class NMSUtil {
 	 * @return Minor server version, for example '14' from 'v1_14_R1'.
 	 */
 	public static int getMinorVersion() {
-		return MINOR_VERSION;
+		return MINOR;
+	}
+
+	/**
+	 * @return Reelase version, for example '2' from 'v1_16_R2'.
+	 */
+	public static int getReleaseVersion() {
+		return RELEASE;
 	}
 
 	public static Class<?> getNMSClass(@NonNull String name) throws ClassNotFoundException {
