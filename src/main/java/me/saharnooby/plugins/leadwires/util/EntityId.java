ce9b041a161cc2ccabb9625ec48add6e05fc77aa
saharNooby <saharnooby@protonmail.com>
2022-06-17 17:38:47 +0400

    Search for next entity ID field automatically on 1.17+

--- src/main/java/me/saharnooby/plugins/leadwires/util/EntityId.java
+++ src/main/java/me/saharnooby/plugins/leadwires/util/EntityId.java
@@ -3,6 +3,7 @@ package me.saharnooby.plugins.leadwires.util;
 import me.saharnooby.plugins.leadwires.LeadWires;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 
@@ -20,7 +21,25 @@ public final class EntityId {
 		try {
 			boolean v17 = NMSUtil.getMinorVersion() >= 17;
 
-			Field field = NMSUtil.getNMSClass(v17 ? "world.entity.Entity" : "Entity").getDeclaredField(v17 ? "b" : "entityCount");
+			Class<?> entityClass = NMSUtil.getNMSClass(v17 ? "world.entity.Entity" : "Entity");
+
+			Field field = null;
+
+			if (v17) {
+				for (Field f : entityClass.getDeclaredFields()) {
+					if (f.getType() == AtomicInteger.class && (f.getModifiers() & Modifier.STATIC) != 0) {
+						field = f;
+
+						break;
+					}
+				}
+
+				if (field == null) {
+					throw new IllegalStateException("AtomicInteger field not found in Entity class");
+				}
+			} else {
+				field = entityClass.getDeclaredField("entityCount");
+			}
 
 			field.setAccessible(true);
 
