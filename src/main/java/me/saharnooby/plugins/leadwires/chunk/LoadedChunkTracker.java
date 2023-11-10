396f66e23ab34f812f02c8b73ae919b08699a9fb
_Tim1 <76794156+LedariaTim1@users.noreply.github.com>
2023-11-10 18:29:15 +0100

    Update src/main/java/me/saharnooby/plugins/leadwires/chunk/LoadedChunkTracker.java
    Co-authored-by: Alex <saharNooby@users.noreply.github.com>

--- src/main/java/me/saharnooby/plugins/leadwires/chunk/LoadedChunkTracker.java
+++ src/main/java/me/saharnooby/plugins/leadwires/chunk/LoadedChunkTracker.java
@@ -88,7 +88,6 @@ public final class LoadedChunkTracker extends PacketAdapter implements Listener 
                 ChunkCoord coord;
                 if (NMSUtil.getMinorVersion() >= 20 && NMSUtil.getReleaseVersion() > 1) {
                     coord = new ChunkCoord(packet.getStructures().read(0).getIntegers().read(0), packet.getStructures().read(0).getIntegers().read(1));
-
                 } else {
                     coord = new ChunkCoord(packet.getIntegers().read(0), packet.getIntegers().read(1));
                 }
